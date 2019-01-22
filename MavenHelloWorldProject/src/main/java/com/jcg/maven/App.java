859b2f3ba8c5c84b61fb02359684de017e9c8cd2
dvandyke723 <46197494+dvandyke723@users.noreply.github.com>
2019-01-22 11:20:42 -0500

    Update App.java

--- MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
+++ MavenHelloWorldProject/src/main/java/com/jcg/maven/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Infosys!" );
+        System.out.println( "Hello Infosys World!" );
     }
 }
